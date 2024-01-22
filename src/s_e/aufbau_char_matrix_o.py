f718b49622e1e82f654a69b32a6b304ab50e4372
Thorsten Vitt <thorsten.vitt@uni-wuerzburg.de>
2024-01-22 16:12:48 +0100

    chore: Refactored aufbau_car_matrix_o + Tests

--- src/s_e/aufbau_char_matrix_o.py
+++ src/s_e/aufbau_char_matrix_o.py
@@ -1,69 +1,30 @@
 from collections import Counter
-#from collections import OrderedDict
-import timeit
-
-#from time  import Timer
+from collections.abc import Iterable
+from pathlib import Path
 
 import pandas as pd
-from pathlib import Path
-import glob
-#defaultdict wuerd ersetzt
-import numpy as np
-import sys
-import pprint
 
 
-corpus_dir_name = 'Romane2'
-projekt_pfad = 'C:\\users\\Eickelpasch\\Documents\\corpuschars'
-corpus_pfad = projekt_pfad + '\\' + corpus_dir_name
-#print (corpus_pfad)
-cp = Path(corpus_pfad)
-print (cp)
-t_start = timeit.default_timer()
+def analyze_text(text: str) -> Counter:
+    return Counter(text)
 
-corpusfiles = list(cp.glob("*.txt")) #  gab immer den gesamten pfad zurueck
-def erzeuge_liste_corpusflenames (corpusfiles):
-    """
-    erzeuge eine liste mit den corpusfile names
-    :param corpusfiles:
-    :return: corpusfilename: liste mit den corpusfile names.
-    """
-    corpusfilename = []
-    for i in range(len(corpusfiles)):
-        corpusfilename.append(Path(corpusfiles[i]).name)
-    return corpusfilename
-corpusfilenames = erzeuge_liste_corpusflenames(corpusfiles)
-print (corpusfilenames)
-char_matrix = pd.DataFrame()
-for j in range(len(corpusfiles)):
-    cp_file  = Path(corpusfiles[j])
-    text = cp_file.read_text("locale")
-   # text = cp_file.read_text(encoding = "UTF-8")
-    '''
-    "locale" sthet für das encoding- defautl ist wohl utf-8 , das einen Fehler erzeugte
-    aber auch fand ich in 'Hauff,-Wilhelm_Die Bettlerin vom Pont des Arts.txt' die
-    gleiche Fehlermeldung. Genauer wurde gemeldet
-    File "C:\Users\Eickelpasch\AppData\Local\Programs\Python\Python311\Lib\encodings\cp1252.py", line 23, in decode
-    return codecs.charmap_decode(input,self.errors,decoding_table)[0]
-           ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-UnicodeDecodeError: 'charmap' codec can't decode byte 0x81 in position 815560: character maps to <undefined>
-Eine Position 815560 gibt es aber nicht ( 231 220 ist die letze laut notepad++ und
-0x81 wird in notepad++ nicht gefunden.
-    '''
-    cnt = Counter()
-    for character in text:
-        cnt[character] += 1
-    if j == 0 :
-        char_matrix = pd.DataFrame.from_dict(cnt, orient= 'index',columns = [0])
-        print (char_matrix)
-    else:
-        char_matrix_r = pd.DataFrame.from_dict(cnt, orient= 'index',columns = [j])
-        char_matrix = char_matrix.join(char_matrix_r,on=None,how='outer')
+def analyze_file(file: Path) -> Counter:
+    return analyze_text(file.read_text(encoding='utf-8'))
 
-char_matrix.fillna(0,inplace=True)
-char_matrix['Summe'] = char_matrix.sum(axis=1)
-charmatrix_sorted = char_matrix.sort_values(by='Summe', ascending=True)
-t_stop = timeit.default_timer()
-print (charmatrix_sorted.shape)
-print ("Gesamtlaufzeit = " + str(t_stop - t_start))
-#print (cnt)
+def analyze_files(corpusfiles: Iterable[Path]) -> pd.DataFrame:
+    analyzed_data = {}   # { file : Counter({char : count})}
+    for file in corpusfiles:
+        analyzed_data[file.stem] = analyze_file(file)
+    char_matrix = pd.DataFrame(analyzed_data)
+
+    char_matrix.fillna(0,inplace=True)
+    char_matrix['Summe'] = char_matrix.sum(axis=1)
+    charmatrix_sorted = char_matrix.sort_values(by='Summe', ascending=True)
+    return charmatrix_sorted
+
+def main():
+    matrix = analyze_files(Path('/home/tv/git/corpuschars/tests/data/mini').glob('*.txt'))
+    print(matrix)
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_aufbau_char_matrix_o.py
@@ -0,0 +1,45 @@
+from collections import Counter
+from pathlib import Path
+
+import pytest
+
+from s_e.aufbau_char_matrix_o import analyze_text, analyze_files
+
+
+def test_analyze_text():
+    output = analyze_text("Das ist ein Test")
+    expected = Counter("Das ist ein Test")
+    assert output == expected
+def test_analyze_text_0():
+    output = analyze_text("Otto")
+    expected = Counter({"O": 1, "t": 2, "o": 1})
+    assert output == expected
+
+def test_analyze_text_1():
+    assert analyze_text("") == {}
+
+@pytest.mark.xfail(reason="Not implemented yet")
+def test_analyze_text_2():
+    emoticon = "\U0001FAF4\U0001F3FF"
+    print(emoticon)
+    table = analyze_text(emoticon)
+    assert len(table) == 1, "emoji should count as one"
+
+@pytest.fixture()
+def data_folder():
+    return Path(__file__).parent / 'data'
+
+def test_analyze_files(data_folder):
+    files = data_folder.joinpath("mini").glob("*.txt")
+    result = analyze_files(files)
+    assert len(result.index) > 3
+    assert len(result.columns) == 3
+
+def test_analyze_many_files(data_folder):
+    path = data_folder.joinpath("romane/German")
+    print(path)
+    files = path.glob("*.txt")
+    assert files
+    result = analyze_files(files)
+    assert len(result.index) > 3
+    assert len(result.columns) == 76
